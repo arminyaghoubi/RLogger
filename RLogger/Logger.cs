e30a6e0039e903bc1264d546ca192b233e6be60f
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-06-03 12:07:24 +0330

    Add project files.

---
+++ RLogger/Logger.cs
@@ -0,0 +1,45 @@
+﻿namespace RLogger;
+
+public sealed class Logger
+{
+    private readonly string _filePath;
+    private readonly int _batchSize;
+    private readonly List<string> _logs = new List<string>();
+
+    public Logger(string filePath, int batchSize = 1)
+    {
+        _filePath = filePath;
+        _batchSize = batchSize;
+    }
+
+    public void Log(string message)
+    {
+        _logs.Add(message);
+        if (_logs.Count >= _batchSize)
+        {
+            _=WriteAsync();
+        }
+    }
+
+    private async Task WriteAsync()
+    {
+        List<string> logsToWrite;
+
+        lock (_logs)
+        {
+            logsToWrite = new List<string>(_logs);
+            _logs.Clear();
+        }
+
+        await Task.Run(() =>
+        {
+            using (var writer = new StreamWriter(_filePath,false))
+            {
+                foreach (var log in logsToWrite)
+                {
+                    writer.WriteLine($"{DateTime.Now}: {log}");
+                }
+            }
+        });
+    }
+}
---
+++ TestApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using RLogger;
+
+Logger logger = new("logs.txt", 1000);
+
+for (int i = 1; i < 1300; i++)
+{
+    logger.Log($"Log {i}");
+}
+
+Console.WriteLine("Finished");
+
+Console.ReadKey();
